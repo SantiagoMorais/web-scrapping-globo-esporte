b846d6d124adcf2c0f7d1c06f76dd584506ef80b
Felipe <fillikz@gmail.com>
2025-05-18 17:52:29 -0300

    fix(tests): correct test description for FetchActiveGamesUseCase

--- src/domain/application/use-cases/fetch-active-games.test.ts
+++ src/domain/application/use-cases/fetch-active-games.test.ts
@@ -5,13 +5,13 @@ import { FetchActiveGamesUseCase } from "./fetch-active-games";
 let inMemoryGamesRepository: InMemoryGamesRepository;
 let sut: FetchActiveGamesUseCase;
 
-describe("Finish Game Use Case", () => {
+describe("Fetch Active Games Use Case", () => {
   beforeEach(() => {
     inMemoryGamesRepository = new InMemoryGamesRepository();
     sut = new FetchActiveGamesUseCase(inMemoryGamesRepository);
   });
 
-  it("should be able to create a new game", async () => {
+  it("should be able to fetch active games", async () => {
     await inMemoryGamesRepository.create(makeGame()); // the isOver property is set to false by default when the game is created
     await inMemoryGamesRepository.create(makeGame({ isOver: true }));
     await inMemoryGamesRepository.create(makeGame());
