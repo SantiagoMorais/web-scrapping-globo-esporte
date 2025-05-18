882e1c6937fd308f9567e6209291c6a2706300e8
Felipe <fillikz@gmail.com>
2025-05-18 16:06:33 -0300

    feat(entities): implement Game class with properties and creation logic

---
+++ src/core/types/entities/game-props.ts
@@ -0,0 +1,8 @@
+export type TGameProps = {
+    id: string;
+    title: string;
+    link: string;
+    isOver: boolean;
+    createdAt: Date;
+    updatedAt?: Date;
+}
---
+++ src/domain/enterprise/entities/game.ts
@@ -0,0 +1,42 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { TGameProps } from "@/core/types/entities/game-props";
+import { Optional } from "@/core/types/optional";
+
+export class Game extends Entity<TGameProps> {
+  get title() {
+    return this.props.title;
+  }
+
+  get link() {
+    return this.props.link;
+  }
+
+  get isOver() {
+    return this.props.isOver;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  static create(props: Optional<TGameProps, "createdAt">, id?: UniqueEntityId) {
+    const game = new Game(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id
+    );
+
+    return game;
+  }
+}
